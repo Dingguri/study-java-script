e412f7fcf4ed72cabef9585b115051534764d315
PARK SEUNG MIN <min62156347@gmail.com>
2019-12-24 22:11:13 +0900

    Submit and Set item in local Storage

--- Momentum/greeting.js
+++ Momentum/greeting.js
@@ -5,19 +5,35 @@ const greeting = document.querySelector(".js-greeting");
 
 const USER_LS = "currentUser";
 const SHOWING_CN = "showing";
-const FROM_CN = "from";
+
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); // 기본 이벤트 동작하지 않도록 함
+    const curValue = input.value;
+    paintGreeting(curValue);
+    saveName(curValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit);
+}
 
 function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello ${text}`;
 }
 
 function loadName() {
-    const currentUser = localStorage.getItem(USER_LS);
-    if (currentUser === null) {
-
+    const curUser = localStorage.getItem(USER_LS);
+    if (curUser === null) {
+        askForName();
     } else {
-        paintGreeting(currentUser);
+        paintGreeting(curUser);
     }
 }
 
