667ada84839f3e11ab3c0a67ec3bc9a968973bd4
PARK SEUNG MIN <min62156347@gmail.com>
2020-01-06 21:51:15 +0900

    ToDos Save As json
    ToDos 를 json 형식으로 local Storage에 저장

--- Momentum/todo.js
+++ Momentum/todo.js
@@ -4,22 +4,40 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = 'toDos';
 
-function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if (toDos !== null) {
+const toDos = [];
 
+function loadToDos() {
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text);
+        });
     }
+}
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
 function paintToDo(text) {
     const li = document.createElement("li");
     const delBtn = document.createElement("button");
-    delBtn.innerText = "Delete";
     const span = document.createElement("span");
+    const newId = toDos.length + 1;
+    delBtn.innerText = "Delete";
     span.innerText = text;
     li.appendChild(delBtn);
     li.appendChild(span);
+    li.id = newId;
     toDoList.appendChild(li);
+
+    const toDoObj = {
+        text: text,
+        id: newId,
+    };
+    toDos.push(toDoObj);
+    saveToDos();
 }
 
 function handleSubmit(event) {
