c4bcadce2cd2af0a736fb48332229907de5f9e6a
PARK SEUNG MIN <min62156347@gmail.com>
2020-01-12 19:00:52 +0900

    Show Temperature and Place

--- Momentum/weather.js
+++ Momentum/weather.js
@@ -1,4 +1,17 @@
+const weather = document.querySelector(".js-weather");
+
 const COORDS = 'coords';
+const API_KEY = "90f7de39dea64d5eb8e6f609a041d178";
+
+function getWeather(lat, lon) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response) {
+        return response.json();
+    }).then(function(json) {
+        const temperature = json.main.temp;
+        const place = json.name;
+        weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
     localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +25,7 @@ function handleGeoSucces(position) {
         longitude,
     };
     saveCoords(coordsObj)
+    getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,7 +41,8 @@ function loadCoords() {
     if (loadedCoords === null) {
         askForCoords();
     } else {
-
+        const parsedCoords = JSON.parse(loadedCoords);
+        getWeather(parsedCoords.latitude, parsedCoords.longitude);
     }
 }
 
